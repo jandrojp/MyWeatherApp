a71b89a0e8cc73ba9d4e0a29b2fcfa04f2e0a5cb
alejandro jorge <alejorpas2@alu.edu.gva.es>
2024-04-24 02:06:06 +0200

    Commit

--- app/src/main/java/com/example/myweatherbase/activities/DetailsActivity.java
+++ app/src/main/java/com/example/myweatherbase/activities/DetailsActivity.java
@@ -17,6 +17,14 @@ public class DetailsActivity extends AppCompatActivity {
     private FloatingActionButton buttonReturn;
     private TextView grados;
     private TextView sensacionTermica;
+    private TextView descripcion;
+    private TextView temperaturaMinima;
+    private TextView temperaturaMaxima;
+    private TextView humedad;
+    private TextView probLluvia;
+    private TextView presion;
+    private TextView velViento;
+    private TextView visibilidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,14 @@ public class DetailsActivity extends AppCompatActivity {
         buttonReturn = findViewById(R.id.buttonReturn);
         grados = findViewById(R.id.grados);
         sensacionTermica = findViewById(R.id.sensacionTermica);
+        descripcion = findViewById(R.id.descripcionTiempo);
+        temperaturaMinima = findViewById(R.id.tempMinima);
+        temperaturaMaxima = findViewById(R.id.tempMaxima);
+        humedad = findViewById(R.id.humedad);
+        probLluvia = findViewById(R.id.probLluvia);
+        presion = findViewById(R.id.presion);
+        velViento = findViewById(R.id.velViento);
+        visibilidad = findViewById(R.id.visibilidad);
 
 
         Root root = (Root) getIntent().getExtras().get("root");
@@ -35,6 +51,14 @@ public class DetailsActivity extends AppCompatActivity {
         ImageDownloader.downloadImage(Parameters.ICON_URL_PRE + root.list.get(position).weather.get(0).icon + Parameters.ICON_URL_POST, clima);
         grados.setText((int)root.list.get(position).main.temp + "º");
         sensacionTermica.setText((int)root.list.get(position).main.feels_like + "º C");
+        descripcion.setText(root.list.get(position).weather.get(0).description.substring(0, 1).toUpperCase() + root.list.get(position).weather.get(0).description.substring(1));
+        temperaturaMinima.setText((int)root.list.get(position).main.temp_min + "º");
+        temperaturaMaxima.setText((int)root.list.get(position).main.temp_max + "º");
+        humedad.setText(root.list.get(position).main.humidity + "%");
+        probLluvia.setText(root.list.get(position).rain + "%");
+        presion.setText(root.list.get(position).main.pressure + " mB");
+        velViento.setText((int)root.list.get(position).wind.speed + " km/h");
+        visibilidad.setText(root.list.get(position).visibility + "m");
 
         buttonReturn.setOnClickListener(view -> finish());
     }
